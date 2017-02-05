5086df8d1ef32291374bde7cb17ed7128e3b2cd0
JonghunYu <yuzong@naver.com>
2017-02-05 20:46:46 +0900

    set basic kakaotalk autoreply

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', (req, res, _) => {
   res.render('index', {title: 'Express'});
 });
 
+router.get('/keyboard', (req, res, _) => {
+  console.log(req.body);
+  res.json({
+    type: 'buttons',
+    buttons: ['선택 1', '선택 2', '선택 3']
+  });
+});
+
+router.post('/message', (req, res, _) => {
+  console.log(req.body);
+  res.json({
+    message: {
+      text : '감사합니다.'
+    }
+  });
+});
+
+router.delete('/friend/:userKey', (req, res, _) => {
+  console.log('user disconnected!: ', req.params.userKey);
+  console.log(req.body);
+  res.json({});
+});
+
+router.delete('/chat_room/:userKey', (req, res, _) => {
+  console.log('user closed chat room!: ', req.params.userKey);
+  console.log(req.body);
+  res.json({});
+});
+
 module.exports = router;
